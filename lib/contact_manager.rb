058210a477cc6c4b2269563f8b43dc2bb5f3341f
Justin Bowen <JusBowen@gmail.com>
2016-03-04 11:19:22 -0800

    Adding initializer that parses contacts and sets contacts_by_email

--- lib/contact_manager.rb
+++ lib/contact_manager.rb
@@ -1,2 +1,23 @@
+require 'csv'
+
 class ContactManager
+  def initialize(contacts_csv)
+    @contacts_by_email = parse_contacts(contacts_csv)
+  end
+
+  def contacts_by_email
+    @contacts_by_email ||= {}
+  end
+
+  protected
+  def parse_contacts(contacts_csv)
+    keys = %w{ first_name last_name email phone }
+    contacts = CSV.parse(contacts_csv).map {|a| Hash[ keys.zip(a) ] }
+
+    for contact in contacts
+      contacts_by_email[contact['email'].downcase] = contact
+    end
+
+    contacts_by_email
+  end
 end
